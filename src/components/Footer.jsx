4b3018e45dc292af8ca4c3a56539ca4be913047a
Simon Mekonnen Legesse <103356546+Leviathan913@users.noreply.github.com>
2024-12-28 22:27:46 +0300

    updated phone number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>+251938492738</li>
+            <li>+251911223344</li>
             <li>contact@smartGadges.com</li>
           </ul>
         </div>
