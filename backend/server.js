6bbbb79fbc145ed119c63173170c2bb302b85b74
Simon Mekonnen Legesse <103356546+Leviathan913@users.noreply.github.com>
2025-01-28 12:31:55 +0300

    register and login

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDb();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log("Server started on PORT : " + port));
